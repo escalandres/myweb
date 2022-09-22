52ca9a7dfb97b7cee5e4f7d3f73be9adce573fe4
escalandres <aescalaa1900@alumno.ipn.mx>
2022-09-22 00:27:22 -0500

    Fontawesome icons import and password eye icon added

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
+import 'bulma/css/bulma.min.css';
 import '../css/Auth.css'
 import cyber from '../assets/cyber2.png'
+// import { Carousel, ProgressBar, Button, DropdownButton, Dropdown, Alert, ToastContainer, Toast, Modal, ModalHeader, ModalBody, ModalFooter} from 'react-bootstrap';
+// import * from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
+import { faEyeSlash } from "@fortawesome/free-brands-svg-icons"
+import { faDownload } from '@fortawesome/fontawesome-free-solid';
 
 function mostrarContrasena(){
     var pass = document.getElementById("password");
@@ -42,7 +50,7 @@ function Auth(){
                             <label id="login-lpassword" className="s-mb-1 s-weight-semibold small">Contraseña</label>
                             <div className="input-group">
                                 <input id="password" type="password" name="password" className="form-control" placeholder="Escribe tu contraseña" required/>
-                                <button id="log_show_password" className="btn btn-primary" type="button" onClick={mostrarContrasena}><span className="fa fa-eye-slash icon"></span></button>
+                                <button id="log_show_password" className="btn btn-primary" type="button" onClick={mostrarContrasena}><FontAwesomeIcon className="fa-solid" icon="fa-eye-slash"/></button>
                                 
                             </div>
                         </div>
@@ -50,7 +58,7 @@ function Auth(){
                             <label id="login-key" className="s-mb-1 s-weight-semibold small">Contraseña</label>
                             <div className="input-group">
                                 <input id="pkey" type="password" name="key" className="form-control" placeholder="Escribe tu key" required/>
-                                <button id="log_show_key" className="btn btn-primary" type="button" onClick={mostrarKey}><span className="fa fa-eye-slash icon"></span></button>
+                                <button id="log_show_key" className="btn btn-primary" type="button" onClick={mostrarKey}><FontAwesomeIcon icon="fa-solid fa-eye" /></button>
                             </div>
                         </div>
                         <button className="w-100 btn btn-lg btn-primary" type="submit">Iniciar sesión</button>
