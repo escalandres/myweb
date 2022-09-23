2a2b329addf258f3ae2335901ceefdd29dcf8f6b
escalandres <aescalaa1900@alumno.ipn.mx>
2022-09-22 23:54:43 -0500

    show-btn css style changed

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -2,13 +2,9 @@ import React, { useState } from 'react';
 import 'bulma/css/bulma.min.css';
 import '../css/Auth.css'
 import cyber from '../assets/cyber2.png'
-// import { Carousel, ProgressBar, Button, DropdownButton, Dropdown, Alert, ToastContainer, Toast, Modal, ModalHeader, ModalBody, ModalFooter} from 'react-bootstrap';
-// import * from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
-import { faEyeSlash } from "@fortawesome/free-brands-svg-icons"
-import { faDownload } from '@fortawesome/fontawesome-free-solid';
 
 function mostrarContrasena(){
     var pass = document.getElementById("password");
@@ -27,7 +23,6 @@ function mostrarKey(){
     }
 }
 
-// const Auth = () => {
 function Auth(){
     return(
         <div className="auth__container">
@@ -39,29 +34,29 @@ function Auth(){
                         </div>
                     </a>
                     <h1 className="t2 s-weight-extrabold s-center s-mb-4">Inicia sesión</h1>
-                    <form action="/login" method="post">
+                    <form action="/" method="post">
                         <div className="input-form s-relative s-mb-2 ">
                             <div className="s-relative s-cross-center">
-                                <label className="s-mb-1 s-weight-semibold small">Username</label>
-                                <input id="log_username" type="text" name="username" className="form-control" placeholder="Escribe tu username" required/>
+                                <label for="username" className="s-mb-1 s-weight-semibold small">Username</label>
+                                <input id="username" type="text" name="username" className="form-control" placeholder="Escribe tu username" required/>
                             </div>
                         </div>
-                        <div className="s-relative s-cross-center">
-                            <label id="login-lpassword" className="s-mb-1 s-weight-semibold small">Contraseña</label>
+                        <div className="input-form s-relative s-cross-center">
+                            <label for="password" id="login-lpassword" className="s-mb-1 s-weight-semibold small">Contraseña</label>
                             <div className="input-group">
                                 <input id="password" type="password" name="password" className="form-control" placeholder="Escribe tu contraseña" required/>
-                                <button id="log_show_password" className="btn btn-primary" type="button" onClick={mostrarContrasena}><FontAwesomeIcon className="fa-solid" icon="fa-eye-slash"/></button>
+                                <button id="log_show_password" className="btn btn-primary show-btn" type="button" onClick={mostrarContrasena}><FontAwesomeIcon className="fa-solid" icon="fa-eye-slash"/></button>
                                 
                             </div>
                         </div>
-                        <div className="s-relative s-cross-center">
-                            <label id="login-key" className="s-mb-1 s-weight-semibold small">Contraseña</label>
+                        <div className="input-form s-relative s-cross-center">
+                            <label for="key" id="login-key" className="s-mb-1 s-weight-semibold small">Contraseña</label>
                             <div className="input-group">
-                                <input id="pkey" type="password" name="key" className="form-control" placeholder="Escribe tu key" required/>
-                                <button id="log_show_key" className="btn btn-primary" type="button" onClick={mostrarKey}><FontAwesomeIcon icon="fa-solid fa-eye" /></button>
+                                <input id="key" type="password" name="key" className="form-control" placeholder="Escribe tu key" required/>
+                                <button id="log_show_key" className="btn btn-primary show-btn" type="button" onClick={mostrarKey}><FontAwesomeIcon icon="fa-solid fa-eye" /></button>
                             </div>
                         </div>
-                        <button className="w-100 btn btn-lg btn-primary" type="submit">Iniciar sesión</button>
+                        <button className="w-100 btn btn-lg btn-primary submit-btn" type="submit">Iniciar sesión</button>
                         <p className="mt-5 mb-3 text-muted">&copy; Escalandres</p>
                     </form>
                 </section>
